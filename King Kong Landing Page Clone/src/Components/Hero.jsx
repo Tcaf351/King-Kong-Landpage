4c338a1279a74bf8a468bb841e923f59c8156d1b
Tcaf351 <74075640+Tcaf351@users.noreply.github.com>
2024-02-06 09:03:33 +1100

    Added background opacity to hero video

--- King Kong Landing Page Clone/src/Components/Hero.jsx
+++ King Kong Landing Page Clone/src/Components/Hero.jsx
@@ -2,20 +2,22 @@ import HeroVideo from '../assets/hero-bg-video-2021.mp4';
 
 const Hero = () => {
     return ( 
-        <div className="h-screen flex flex-col items-center justify-center text-gray-50 bg-blend-darken">
-            <div className='z-30'>
-                <p className="text-9xl font-bold">LIKE STEROIDS<sup>*</sup>
+        <div className="h-screen flex flex-col items-center justify-center text-gray-50">
+            <div className='z-30 w-full justify-self-center px-3.5 mx-auto'>
+                <p className="text-9xl font-bold min-w self-center">LIKE STEROIDS<sup>*</sup>
                 <br /> FOR BUSINESS</p>
 
                 <p>Growing a business is hard. We make it a whole lot easier, more predictable, less stressful, and more fun.</p>
             </div>
 
-            <video src={ HeroVideo }
-            autoPlay
-            loop
-            muted
-            className='absolute z-10 w-auto min-w-full min-h-full max-w-none '
-            />
+            <div className='bg-[#0a0404] absolute w-auto min-w-full min-h-full max-w-none'>
+                <video src={ HeroVideo }
+                autoPlay
+                loop
+                muted
+                className='z-10 opacity-15 w-auto min-w-full min-h-full max-w-none'
+                />
+            </div>
         </div>
      );
 }
